a120b41086d9bbb7cf01f82ab81fc18d3ed514ca
sashimii <stailor@Metroland-Administrators-MacBook-Pro-3.local>
2015-07-01 18:40:45 -0400

    looks a tad sexier now

--- js/capabilities.js
+++ js/capabilities.js
@@ -1,8 +1,46 @@
 (function(){
   var pictures = [
-  	{name:'materials'},
-    {name: 'machinery'},
-  	{name:'industry'}
+  	{
+      name:'materials',
+      content: 'We work with a variety of materials. These include but are not limited to:',
+      items: [
+        'Steel',
+        'Stainless Steel',
+        'Aluminium',
+        'Magnesium',
+        'Titanium',
+        'Plastics',
+        'Delrin',
+        'Inconel',
+        'Brass',
+        'Copper',
+        'etc'
+      ],
+      tab: 'active'
+    },
+    {
+      name: 'machinery',
+      content: 'We house a variety of machinery, ranging from CNC milling and lathe, manual milling and lathe, to machines that help us control the quality of our products, such as CMM.',
+      items: [],
+      tab: ''
+    },
+  	{
+      name:'industry',
+      content: 'Equipped with state of the art machinery, SIGMA METALS’ can supply, but is not limited to: ',
+      items: [
+        'telecommunications',
+        'medical',
+        'industrial',
+        'automation',
+        'transportation',
+        'security',
+        'aerospace',
+        'lighting',
+        'heavy',
+        'machinery'
+      ],
+      tab: ''
+    }
   	];
   var app = angular.module('sigmaApp', []);
   app.controller('SigmaController', function(){
@@ -13,8 +51,12 @@
 
    	// Function to run on click
    	this.switcheroo = function(pictureID) {
-   		this.pictureIndex = pictureID;
+       pictures[this.pictureIndex].tab = '';
+       this.pictureIndex = pictureID;
+       pictures[this.pictureIndex].tab = 'active';
    	}
+
+
 
   });
 })();
